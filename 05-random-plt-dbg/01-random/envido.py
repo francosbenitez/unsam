5a4d6bb9cc42c4be8d4a501c3d0fdcdfc49fd1aa
Franco Sebastián Benítez <62864550+francosbenitez@users.noreply.github.com>
2021-04-09 21:39:39 -0300

    Delete unsam directory

--- 05-random-plt-dbg/01-random/envido.py
+++
@@ -1,8 +0,0 @@
-"""
-Ejercicio 5.3: Envido
-Teniendo en cuenta las reglas del Truco, estimá la probabilidad de obtener 31, 32 o 33 puntos de envido en una mano. ¿Son iguales estas tres probabilidades? ¿Por qué?
-
-Observación: como corresponde, en esta materia jugamos al truco sin flor. Si no conocés las reglas del Truco y no te dan ganas de aprenderlo ahora, simplemente salteá este ejercicio.
-
-Guardá este ejercicio en un archivo envido.py para entregar.
-"""
--- 05-random-plt-dbg/02-numpy/guardar-temperaturas.py
+++
@@ -1,6 +0,0 @@
-"""
-Ejercicio 5.7: Guardar temperaturas
-Ampliá el código de termometro.py que escribiste en el Ejercicio 5.5 para que guarde el vector con las temperaturas simuladas en el directorio Data de tu carpeta de ejercicios, en un archivo llamado Temperaturas.npy. Hacé que corra 999 veces en lugar de solo 99.
-"""
-
-
