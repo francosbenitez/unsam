60987812643ead25bbdaf631fb3bb002193cd339
francosbenitez <francosbenitez@gmail.com>
2021-04-10 22:56:04 -0300

    Update cuantos-paquetes.py

--- 05-random-plt-dbg/03-figuritas/cuantos-paquetes.py
+++ 05-random-plt-dbg/03-figuritas/cuantos-paquetes.py
@@ -4,10 +4,34 @@ Implementá una función cuantos_paquetes(figus_total, figus_paquete) que dado e
 
 Ejercicio 5.18:
 Calculá n_repeticiones = 100 veces la función cuantos_paquetes, utilizando figus_total = 670, figus_paquete = 5. Guarda los resultados obtenidos en una lista y calculá su promedio. Si te da la compu, hacelo con 1000 repeticiones.
+
+Graficar el llenado del álbum
+El siguiente código usa las funciones que hiciste antes para graficar la curva de llenado de un álbum a medida que comprás paquetes de figuritas. Es un primer ejemplo de gráfico de líneas. En las próximas clases estudiaremos los detalles sobre gráficos de una manera sistemática. Por ahora solo un botón de muestra.
+
+def calcular_historia_figus_pegadas(figus_total, figus_paquete):
+    album = crear_album(figus_total)
+    historia_figus_pegadas = [0]
+    while album_incompleto(album):
+        paquete = comprar_paquete(figus_total, figus_paquete)
+        while paquete:
+            album[paquete.pop()] = 1
+        figus_pegadas = (album>0).sum()
+        historia_figus_pegadas.append(figus_pegadas)        
+    return historia_figus_pegadas
+
+figus_total = 670
+figus_paquete = 5
+
+plt.plot(calcular_historia_figus_pegadas(figus_total, figus_paquete))
+plt.xlabel("Cantidad de paquetes comprados.")
+plt.ylabel("Cantidad de figuritas pegadas.")
+plt.title("La curva de llenado se desacelera al final")
+plt.show()
 """
 
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 def crear_album(figus_total):
     figus_total = np.zeros(figus_total, dtype=np.int64)
@@ -46,3 +70,23 @@ for i in range(n_repeticiones):
 
 promedio = np.mean(resultados)
 print(promedio)
+
+def calcular_historia_figus_pegadas(figus_total, figus_paquete):
+    album = crear_album(figus_total)
+    historia_figus_pegadas = [0]
+    while album_incompleto(album):
+        paquete = comprar_paquete(figus_total, figus_paquete)
+        while paquete:
+            album[paquete.pop()] = 1
+        figus_pegadas = (album>0).sum()
+        historia_figus_pegadas.append(figus_pegadas)        
+    return historia_figus_pegadas
+
+figus_total = 670
+figus_paquete = 5
+
+plt.plot(calcular_historia_figus_pegadas(figus_total, figus_paquete))
+plt.xlabel("Cantidad de paquetes comprados.")
+plt.ylabel("Cantidad de figuritas pegadas.")
+plt.title("La curva de llenado se desacelera al final")
+plt.show()
