f50e14527f08eeb1508e020fc2ef475946daf3cb
francosbenitez <francosbenitez@gmail.com>
2021-04-12 12:19:21 -0300

    update! :bomb:

--- 05-random-plt-dbg/03-figuritas/cuantos-paquetes.py
+++ 05-random-plt-dbg/03-figuritas/cuantos-paquetes.py
@@ -34,42 +34,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def crear_album(figus_total):
-    figus_total = np.zeros(figus_total, dtype=np.int64)
-    return figus_total
+    return np.zeros(figus_total, dtype=np.int64)
 
-def album_incompleto(A):
-    return not A.all()
+def album_incompleto(album):
+    return 0 in album
 
 def comprar_figu(figus_total):
-    return random.randint(0, figus_total)
+    return random.randint(1, figus_total)-1
 
 def comprar_paquete(figus_total, figus_paquete):
-    paquete = list()
-    for i in range(figus_paquete):
-        lote = comprar_figu(figus_total)
-        paquete.append(lote)
+    paquete = []
+    for _ in range(figus_paquete):
+        paquete.append(comprar_figu(figus_total))
     return paquete
 
 def cuantos_paquetes(figus_total, figus_paquete):
-    cant_compras = 0
-    album = crear_album(figus_total) 
-    while album_incompleto(album) == True:
+    # cant_compras = 0
+    # album = crear_album(figus_total) 
+    # while album_incompleto(album) == True:
+    #     paquete = comprar_paquete(figus_total, figus_paquete)
+    #     cant_compras += 1
+    #     for i in range(figus_paquete-1):
+    #         album[paquete[i]-1] += 1
+    # return cant_compras
+    album = crear_album(figus_total)
+    while album_incompleto(album):
         paquete = comprar_paquete(figus_total, figus_paquete)
-        cant_compras += 1
-        for i in range(figus_paquete-1):
-            album[paquete[i]-1] += 1
-    return cant_compras
+        while paquete:
+            album[paquete.pop()] += 1
+    return album.sum()/figus_paquete
 
-n_repeticiones = 100
 figus_total = 670
+n_repeticiones = 100
 figus_paquete = 5
-resultados = []
-for i in range(n_repeticiones):
-    lote = cuantos_paquetes(figus_total, figus_paquete)
-    resultados.append(lote)
 
+resultados = []
+for _ in range(n_repeticiones):
+    resultados.append(cuantos_paquetes(figus_total, figus_paquete))
+    
 promedio = np.mean(resultados)
-print(promedio)
+print(f"Para llenar un album de {figus_total} figus compré en promedio {promedio} figus ({n_repeticiones} repeticiones)")
 
 def calcular_historia_figus_pegadas(figus_total, figus_paquete):
     album = crear_album(figus_total)
@@ -86,7 +90,12 @@ figus_total = 670
 figus_paquete = 5
 
 plt.plot(calcular_historia_figus_pegadas(figus_total, figus_paquete))
-plt.xlabel("Cantidad de paquetes comprados.")
-plt.ylabel("Cantidad de figuritas pegadas.")
+plt.xlabel("Cantidad de paquetes comprados")
+plt.ylabel("Cantidad de figuritas pegadas")
 plt.title("La curva de llenado se desacelera al final")
 plt.show()
+
+
+
+
+
