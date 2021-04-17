3ce23ea24d312d5521518ba6721c54256cbbb256
francosbenitez <francosbenitez@gmail.com>
2021-04-16 23:12:01 -0300

    Create un-poco-mas-alla.py

---
+++ 06-organizacion-y-complejidad/04-modulos/un-poco-mas-alla.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 6.12: Un poco más allá
+En Ejercicio 2.6 escribiste el programa costo_camion.py que lee, mediante una función llamada costo_camion() los datos de un camión y calcula su costo.
+
+>>> import costo_camion
+>>> costo_camion.costo_camion('../Data/camion.csv')
+47671.15
+>>>
+Modificá el archivo costo_camion.py para que use la función informe.leer_camion() del programa informe_funciones.py.
+"""
+
+
