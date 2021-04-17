0adaf938de56565fb84831092a6c94b8d44c2d1b
francosbenitez <francosbenitez@gmail.com>
2021-04-16 23:24:51 -0300

    :bomb: :bomb: :bomb: :bomb: :bomb:

--- 06-organizacion-y-complejidad/04-modulos/un-poco-mas-alla.py
+++ 06-organizacion-y-complejidad/04-modulos/un-poco-mas-alla.py
@@ -9,4 +9,18 @@ En Ejercicio 2.6 escribiste el programa costo_camion.py que lee, mediante una fu
 Modificá el archivo costo_camion.py para que use la función informe.leer_camion() del programa informe_funciones.py.
 """
 
+import informe_funciones as informe
 
+def costo_camion(nombre_archivo):
+    rows = informe.leer_camion(nombre_archivo)
+    i = 0
+    costo = 0
+    for row in rows:
+        cajones = int(row["cajones"])
+        precio = float(row["precio"])
+        costo += cajones * precio
+        i = i + 1 
+    return costo
+    
+costo = costo_camion("Data/camion.csv")
+print("Costo total:", costo)
