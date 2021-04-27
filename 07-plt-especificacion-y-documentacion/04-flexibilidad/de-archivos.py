115b7d97ef0407b75ef07cfaa1f19cced8da1b31
francosbenitez <francosbenitez@gmail.com>
2021-04-27 10:27:52 -0300

    update

--- 07-plt-especificacion-y-documentacion/04-flexibilidad/de-archivos.py
+++ 07-plt-especificacion-y-documentacion/04-flexibilidad/de-archivos.py
@@ -22,9 +22,68 @@ Y ahora que pasa si le pasás un nombre de archivo como antes ?
 Sí, hay que tener cuidado.
 """
 
-import fileparse
+import csv
 import gzip
+
+def parse_csv(nombre_archivo, select = None, types = None, 
+              has_headers = None, 
+              silence_errors = None):
+    '''
+    Parsea un archivo CSV en una lista de registros
+    '''
+    with open(nombre_archivo) as f:
+            rows = csv.reader(f)
+            if has_headers == True:
+                headers = next(rows)
+                if select:
+                    indices = [headers.index(nombre_columna) for nombre_columna in select]
+                    headers = select
+                else:
+                    indices = []
+                registros = []
+                for i, row in enumerate(rows):
+                    if silence_errors == True: 
+                        try:
+                            if types:
+                                row = [func(val) for func, val in zip(types, row)]
+                            if not row:    
+                                continue
+                            if indices:
+                                row = [row[index] for index in indices]
+                            registro = dict(zip(headers, row))
+                            registros.append(registro)
+                        except ValueError:
+                            pass
+                    else:
+                        try:
+                            if types:
+                                row = [func(val) for func, val in zip(types, row)]
+                            if not row:    
+                                continue
+                            if indices:
+                                row = [row[index] for index in indices]
+                            registro = dict(zip(headers, row))
+                        except ValueError as error:
+                            print(f"Fila {i}: No pude convertir {row}")
+                            print(f"Fila {i}: Motivo:", error)
+                        registros.append(registro)
+            else: 
+                registros = {}
+                for row in rows:
+                    if types:
+                        row = [func(val) for func, val in zip(types, row)]
+                    if not row:   
+                        continue
+                    registros[row[0]] = row[1]
+        
+    return registros
+
+camion = parse_csv('Data/missing.csv', types = [str, int, float], 
+                   has_headers=True, silence_errors = True)
+
 with gzip.open('Data/camion.csv.gz', 'rt') as file:
-    camion = fileparse.parse_csv("Data/camion.csv", types=[str, int, float], 
-                             has_headers=True)
+    camion = parse_csv(file, types = [str,int,float], silence_errors = True)
+    
+lines = ['name,cajones,precio', 'Lima,100,34.23', 'Naranja,50,91.1', 'Mburucuya,75,45.1']
+camion = fileparse.parse_csv(lines, types=[str,int,float], has_headers = False)
 
