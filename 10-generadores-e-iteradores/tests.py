e3d4147763675078362ddf04dc496e88e054abab
francosbenitez <francosbenitez@gmail.com>
2021-05-25 14:19:30 -0300

    testing yield command

---
+++ 10-generadores-e-iteradores/tests.py
@@ -0,0 +1,23 @@
+def regresiva(n):
+    while n > 0:
+        yield n
+        n -= 1
+
+for x in regresiva(10):
+    print(x, end=' ')
+    
+# list(regresiva(10))
+
+#%%
+
+def filematch(filename, substr):
+    with open(filename, 'r') as f:
+        for line in f:
+            if substr in line:
+                yield line
+
+# for line in open('Data/camion.csv'):
+#     print(line, end='')
+        
+for line in filematch('Data/camion.csv', 'Naranja'):
+    print(line, end='')
